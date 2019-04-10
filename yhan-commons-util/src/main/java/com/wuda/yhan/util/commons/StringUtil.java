e1fb42a49af6606d2153a6ea9bba76b8b23e6d1b
wuda <wud@tututech.net>
2019-04-10 16:37:17 +0800

    调整pom，为发布到中央仓库做准备

--- yhan-commons-util/src/main/java/com/wuda/yhan/util/commons/StringUtil.java
+++ yhan-commons-util/src/main/java/com/wuda/yhan/util/commons/StringUtil.java
@@ -22,8 +22,7 @@ public class StringUtil {
     /**
      * 将第一个字符大写.
      *
-     * @param str
-     *         字符串
+     * @param str 字符串
      * @return 新的字符串
      */
     public static String firstCharToUpperCase(String str) {
@@ -33,8 +32,7 @@ public class StringUtil {
     /**
      * 将第一个字符小写.
      *
-     * @param str
-     *         字符串
+     * @param str 字符串
      * @return 新的字符串
      */
     public static String firstCharToLowerCase(String str) {
@@ -44,12 +42,9 @@ public class StringUtil {
     /**
      * 改变指定位置字符的大小写.
      *
-     * @param str
-     *         string
-     * @param index
-     *         位置
-     * @param lowerOrUpper
-     *         lower or upper
+     * @param str          string
+     * @param index        位置
+     * @param lowerOrUpper lower or upper
      * @return new string
      */
     public static String changeCaseAt(String str, int index, CaseEnum lowerOrUpper) {
@@ -80,10 +75,8 @@ public class StringUtil {
     /**
      * 为字符串添加给定的前缀.
      *
-     * @param str
-     *         原始字符串
-     * @param prefix
-     *         前缀
+     * @param str    原始字符串
+     * @param prefix 前缀
      * @return 前缀与原始字符串组成的新字符串
      */
     public static String addPrefix(String str, String prefix) {
@@ -107,13 +100,37 @@ public class StringUtil {
     /**
      * 为字符串添加给定的后缀.
      *
-     * @param str
-     *         原始字符串
-     * @param suffix
-     *         后缀
+     * @param str    原始字符串
+     * @param suffix 后缀
      * @return 原始字符串与后缀组成的新字符串
      */
     public static String addSuffix(String str, String suffix) {
         return addPrefix(suffix, str);
+    }
+
+    /**
+     * 去除掉最后一个char.
+     *
+     * @param charSequence char sequence
+     * @return 去除最后一个字符后的字符串
+     */
+    public static String removeLastChar(CharSequence charSequence) {
+        if (charSequence == null) {
+            return null;
+        } else if (charSequence.length() <= 1) {
+            return EMPTY_STRING;
+        } else {
+            return charSequence.subSequence(0, lastCharIndex(charSequence)).toString();
+        }
+    }
+
+    /**
+     * 最后一个字符的下标.
+     *
+     * @param charSequence char sequence
+     * @return 最后一个字符的下标
+     */
+    public static int lastCharIndex(CharSequence charSequence) {
+        return charSequence.length() - 1;
     }
 }
