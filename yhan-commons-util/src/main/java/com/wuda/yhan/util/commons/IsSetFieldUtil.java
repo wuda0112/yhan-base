b52ddfb196db58c7722471937c7a2929db8c0346
wuda <wud@tututech.net>
2019-05-10 18:14:26 +0800

    命名修改

--- yhan-commons-util/src/main/java/com/wuda/yhan/util/commons/IsSetFieldUtil.java
+++ yhan-commons-util/src/main/java/com/wuda/yhan/util/commons/IsSetFieldUtil.java
@@ -16,12 +16,12 @@ public class IsSetFieldUtil {
      * <strong>注意</strong>必须是使用了{@link IsSetField}特性的类才适用此方法,
      * 否则即使field调用了setter方法,也将永远返回空.
      *
-     * @param domain
-     *         使用了{@link IsSetField}特性的实体类.
+     * @param obj
+     *         使用了{@link IsSetField}特性的类.
      * @return 所有调用过setter方法的属性, null - 如果没有任何field调用过setter方法,或者没有使用{@link IsSetField}特性
      */
-    public static Field[] setterCalledFields(Object domain) {
-        Class<?> clazz = domain.getClass();
+    public static Field[] setterCalledFields(Object obj) {
+        Class<?> clazz = obj.getClass();
         Field[] allFields = clazz.getDeclaredFields();
         if (allFields == null || allFields.length == 0) {
             return null;
@@ -33,7 +33,7 @@ public class IsSetFieldUtil {
                 field.setAccessible(true);
                 boolean isSet;
                 try {
-                    isSet = field.getBoolean(domain);
+                    isSet = field.getBoolean(obj);
                 } catch (IllegalAccessException e) {
                     throw new RuntimeException(e);
                 }
